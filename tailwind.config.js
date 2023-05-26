fd5b9bb6dbb109be8891a50ecd6c38197010cd75
Joe Harrison <joe@sigwinch.uk>
2023-05-26 02:12:08 +0100

    don't interpolate class names

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{pug,js}"],
-  safelist: [{
-    pattern: /(to|from)-(green|orange|blue|yellow|pink|red|fuchsia)-(300|400|500|600)/,
-    variants: ['group-hover', 'dark', 'dark:group-hover'],
-  }],
   theme: {
     fontFamily: {
       sans: '"Nimbus Sans L", "Helvetica Neue", Helvetica, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Arial, "Noto Sans", sans-serif'
