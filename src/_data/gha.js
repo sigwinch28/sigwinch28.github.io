ce8102761e7523511fdd107732218220f490bbaf
Joe Harrison <joe@sigwinch.uk>
2024-05-01 14:08:05 +0100

    use short sha

--- src/_data/gha.js
+++ src/_data/gha.js
@@ -2,8 +2,8 @@ module.exports = function () {
     if (process.env.CI) {
         const run_id = process.env.GITHUB_RUN_ID;
         const run_url = process.env.GITHUB_SERVER_URL + "/" + process.env.GITHUB_REPOSITORY + "/actions/runs/" + run_id;
-        const sha = process.env.GITHUB_SHA;
-        const sha_tree_url = process.env.GITHUB_SERVER_URL + "/" + process.env.GITHUB_REPOSITORY + "/tree/" + sha;
+        const sha = process.env.GITHUB_SHA_SHORT || process.env.GITHUB_SHA;
+        const sha_tree_url = process.env.GITHUB_SERVER_URL + "/" + process.env.GITHUB_REPOSITORY + "/tree/" + process.env.GITHUB_SHA;
         return {
             run_id: run_id,
             run_url: run_url,
@@ -11,11 +11,4 @@ module.exports = function () {
             commit_tree_url: sha_tree_url,
         };
     }
-
-    return {
-        run_id: "1234",
-        run_url: "foo.com/bar",
-        commit: "abc123",
-        commit_tree_url: "foo.com/tree/abc123"
-    };
 };
