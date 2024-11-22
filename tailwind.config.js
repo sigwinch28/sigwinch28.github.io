36166fa911ce832b6ddb77d7d8b5735fa44348b3
Joe Harrison <joe@sigwinch.uk>
2024-11-22 21:26:18 +0000

    lellow

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     extend: {
       animation: {
-        'pulse-full': 'pulse-full 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;'
+        'pulse-full': 'pulse-full 2s cubic-bezier(0.4, 0, 1, 0.6) infinite;'
       }
     },
   },
