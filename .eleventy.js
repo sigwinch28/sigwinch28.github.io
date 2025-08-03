817f39964a22b257ae037dbd43c1eec958c33fdb
Joe Harrison <joe@sigwinch.uk>
2025-08-03 20:45:52 +0100

    new vibes

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy({ './node_modules/@fontsource/orbitron/files/*.woff2': 'files' })
+    eleventyConfig.addPassthroughCopy({ './node_modules/@fontsource/iosevka/files/*.woff2': 'files' })
     // Return your Object options:
     return {
         dir: {
